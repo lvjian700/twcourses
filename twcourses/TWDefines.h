0f11b0961e485675aabc7fa1677816fa3c67a683
lvjian700 <lvjian700@gmail.com>
2013-12-22 21:50:33 +0800

    add #define file.

---
+++ twcourses/TWDefines.h
@@ -0,0 +1,14 @@
+//
+//  TWDefines.h
+//  twcourses
+//
+//  Created by lvjian on 12/22/13.
+//  Copyright (c) 2013 xi'an thoughtworks. All rights reserved.
+//
+
+#ifndef twcourses_TWDefines_h
+#define twcourses_TWDefines_h
+
+#define TWImage(x) [UIImage imageNamed:x]
+
+#endif
