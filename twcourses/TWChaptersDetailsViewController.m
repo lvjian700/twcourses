22550f474530517e1edd5f02cf9139a598319317
Zheng Jianing <jianing.zheng@rea-group.com>
2014-02-27 14:06:44 +0800

    remove unneccessary files

--- twcourses/TWChaptersDetailsViewController.m
+++ twcourses/TWChaptersDetailsViewController.m
@@ -65,7 +65,6 @@
     if (error == nil) {
         [self bindSingleTap];
         [_videoImageView setImage:image];
-        //[_videoImageView addPlayButtonSubView];
     } else {
         DLog(@"--- fetching image fails.");
     }
